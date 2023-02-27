bb0fe3e728239bcc72052b71318a3eb1ace8b804
fahadammar <fahadammar96@outlook.com>
2023-02-27 16:32:40 +0500

    added the info

--- app/src/main/java/com/example/imagesvideosrecycler/model/ImagesRepository.kt
+++ app/src/main/java/com/example/imagesvideosrecycler/model/ImagesRepository.kt
@@ -13,24 +13,32 @@ class ImagesRepository {
     private val imagesDateTime = MediaStore.Images.Media.DATE_TAKEN
 
     // The projection variable specifies which columns we want to retrieve for each image, in this case, the ID and display name columns.
-    val projection = arrayOf(
+    private val projection = arrayOf(
         MediaStore.Images.Media._ID,
         MediaStore.Images.Media.DISPLAY_NAME
     )
 
-    val sortOrder = "$imagesDateTime DESC"
+    private val sortOrder = "$imagesDateTime DESC"
 
     /**
      * MediaStore.Images.Media.DATE_TAKEN is a constant that represents the column name for the date and time when an image was taken or created.
      * This column is available in the MediaStore.Images.Media class and is used to sort and query images based on their capture time.
      *  queryUri variable contains a Uri that points to the internal storage of the device where the images are stored.
      * */
-    val queryUri = MediaStore.Images.Media.INTERNAL_CONTENT_URI
+    private val queryUri = MediaStore.Images.Media.INTERNAL_CONTENT_URI
 
-    val imagePaths = ArrayList<String>()
+    private val imagePaths = ArrayList<String>()
 
     fun getImages(context: Context): ArrayList<String>? {
-        // contentResolver.query() is a method that allows you to perform a query on a ContentProvider, such as the MediaStore ContentProvider in this case.
+        /**
+         * contentResolver.query() is a method that allows you to perform a query on a ContentProvider, such as the MediaStore ContentProvider in this case.
+         * The contentResolver.query() method returns a Cursor object that allows us to iterate over the results of the query.
+         * We can then use the Cursor methods such as getColumnIndexOrThrow() and getString() to retrieve the values for each column for each row
+         * of the results. In this way, we can get information about each image in the internal storage and use it for further processing.
+         *
+         * A Cursor is created by calling the query() method on a ContentResolver object, which returns a Cursor object containing the results of the query.
+         * The Cursor object is then used to retrieve the data from the query result.
+         */
         val cursor = context.contentResolver.query(
             queryUri,
             projection,
@@ -39,6 +47,10 @@ class ImagesRepository {
             sortOrder
         )
 
+        /**
+         * In Android, a Cursor object is a data structure that represents the result of a database query.
+         * It provides a way to access and iterate over the rows of the query result, as well as to retrieve the values of the columns in each row.
+         * */
         cursor?.use {
             val idColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
             val nameColumn = it.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME)
@@ -53,13 +65,17 @@ class ImagesRepository {
             }
         }
 
-        return if (imagePaths.size != 0 || imagePaths != null)
+        // close the cursor after use
+        cursor?.close()
+
+        return if (imagePaths.size != 0)
             imagePaths
         else null
     }
 }
 
 /**
+ * @ContentResolver
  * The `contentResolver.query()` method takes five parameters:
  * (1) The Uri that specifies the location of the data we want to query.
  * (2) The projection array that specifies which columns we want to retrieve. Passing null for this parameter retrieves all columns.
@@ -68,4 +84,22 @@ class ImagesRepository {
  * (5) Passing null for this parameter indicates that we have no placeholders.
  * (6) The sort order for the results, in this case, sortOrder specifies that we want to sort the results in descending order based on the capture time
  * of the images.
+ * */
+
+/**
+ * @Cursor
+ * The Cursor object has a pointer that points to the current row in the result set, and methods that allow you to move the pointer to the next row,
+ * get the values of the columns for the current row, and perform other operations on the data.
+ *
+ * Here are some of the key methods provided by the Cursor class:
+
+ * @moveToFirst(): Moves the cursor to the first row of the result set.
+ * @moveToNext(): Moves the cursor to the next row of the result set.
+ * @getColumnIndex(): Returns the index of the named column in the result set.
+ * @getString(): Returns the value of the specified column as a string.
+ * @getInt(): Returns the value of the specified column as an integer.
+ * @getBlob(): Returns the value of the specified column as a byte array.
+ *
+ * Using a Cursor, you can iterate over the results of a query and retrieve the data that you need to process in your application.
+ * Once you are finished with the Cursor, you should call its close() method to release any resources associated with it.
  * */
